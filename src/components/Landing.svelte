f66c5704a060b61a94be30358872980ff73de267
Angel Echavarria <echavarriasam@gmail.com>
2025-10-14 07:06:04 +0000

    small proto

---
+++ src/components/Landing.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import TwitterCard from '../components/twitter-card/index.svelte';
+	let c = $state({ count: 0 });
+</script>
+
+<main>
+	<h1>Hello, world!</h1>
+	<TwitterCard title="Hello" content="This is a tweet." />
+</main>
+
+<style>
+</style>
---
+++ src/components/twitter-card/index.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let { title = 'title', content = 'content' } = $props();
+</script>
+
+<section class="section">
+	<h2>Title: {title}</h2>
+	<div>Content: {content}</div>
+</section>
+
+<style>
+	.section {
+		padding: 1rem;
+		border: solid blue 1px;
+		border-radius: 0.25rem;
+		margin: 0.5rem 0;
+	}
+</style>
